042c206ad2ad7533d0c0fe924ac2d66092992c75
Vasic2000 <vasic2000@yandex.ru>
2019-06-13 21:09:40 +0300

    Initial commit

---
+++ src/lesson_2/Array.java
@@ -0,0 +1,24 @@
+package lesson_2;
+
+public interface Array<E> {
+    void add(E value);
+
+    E get(int index);
+
+    boolean remove(E value);
+
+    default boolean contains(E value) {
+        return indexOf(value) != -1;
+    }
+
+    int indexOf(E value);
+
+    int size();
+    boolean isEmpty();
+
+    void display();
+
+    void sortBubble();
+    void sortSelect();
+    void sortInsert();
+}
---
+++ src/lesson_2/InvalidArrayStateException.java
@@ -0,0 +1,7 @@
+package lesson_2;
+
+public class InvalidArrayStateException extends RuntimeException {
+    public InvalidArrayStateException(int index, int arraySize) {
+        super(String.format("Invalid index value %d; array size is %d", index, arraySize));
+    }
+}
