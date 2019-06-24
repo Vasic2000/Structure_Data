658d68960d3044b016bc079266488b61d2a9bf61
Vasic2000 <Vasic2000@yandex.ru>
2019-06-24 10:36:41 +0300

    Заработало!!!!!

--- src/Lesson_5/HomeTask/BackPack.java
+++ src/Lesson_5/HomeTask/BackPack.java
@@ -1,48 +1,60 @@
 package Lesson_5.HomeTask;
 
+import java.util.ArrayList;
 import java.util.HashSet;
+import java.util.List;
 import java.util.Set;
 
 public class BackPack {
-    final static int backPackVolume = 6;
+    final static int backPackVolume = 5;
     private Item[] items;
-    private Set<Item[]> result = new HashSet<>();
+    private static Set<Item[]> result = new HashSet<>();
+
+    public static Item[] makeItems() {
+        Item[] items = new Item[5];
+        items[0] = new Item("Книга", 1, 600);
+        items[1] = new Item("Бинокль", 2, 5000);
+        items[2] = new Item("Аптечка", 4, 1500);
+        items[3] = new Item("Ноутбук", 2, 40000);
+        items[4] = new Item("Котелок", 1, 500);
+        return items;
+    }
 
     public BackPack(Item[] items) {
         this.items = items;
     }
 
-    public static void main(String[] args) {
-        new BackPack(makeItems()).getRandomItems();
-    }
-
-
-
-
 
     public void getRandomItems() {
+        Item[] kup = copyOf(items);
         if (items.length == 1)
-            result.add(items);
+            result.add(kup);
         else
             getRandomItems(items.length);
+
+//        for(Item[] it : result) {
+//            for(int i = 0; i < it.length; i++) {
+//                System.out.print(it[i].getName() + " ");
+//            }
+//            System.out.println();
+//        }
+    }
+
+    private Item[] copyOf(Item[] items) {
+        Item[] result = new Item[items.length];
+        for(int i = 0; i < items.length; i++)
+            result[i] = items[i];
+        return result;
     }
 
     private void getRandomItems(int length) {
         if (length == 1) {
-            result.add(items);
+            Item[] kup = copyOf(items);
+            result.add(kup);
         }
-
         for (int i = 0; i < length; i++) {
             getRandomItems(length - 1);
-            result.add(items);
             rotate(length);
-        }
-
-        for(Item[] it : result) {
-            for(int i = 0; i < it.length; i++) {
-                System.out.print(it[i].getName() + " ");
-            }
-            System.out.println();
         }
     }
 
@@ -55,13 +67,48 @@ public class BackPack {
         items[items.length - 1] = temp;
     }
 
-    public static Item[] makeItems() {
-        Item[] items = new Item[5];
-        items[0] = new Item("Книга", 1, 600);
-        items[0] = new Item("Бинокль", 2, 5000);
-        items[0] = new Item("Аптечка", 4, 1500);
-        items[0] = new Item("Ноутбук", 2, 40000);
-        items[0] = new Item("Котелок", 1, 500);
-        return items;
+
+    public static void findExpensive() {
+        int maxPrice = 0;
+        List<Item> maxPriceList = new ArrayList<>();
+
+        int price;
+        int weight;
+        List<Item> priceList = new ArrayList<>();
+
+        for(Item[] it : result) {
+            weight = 0;
+            price = 0;
+            priceList.clear();
+
+            for(int i = 0; i < it.length; i++) {
+                weight += it[i].getWeight();
+                if(weight <= backPackVolume) {
+                    price += it[i].getPrice();
+                    priceList.add(it[i]);
+                }
+            }
+
+            if(price > maxPrice) {
+                maxPrice = price;
+                maxPriceList = copyOf2(priceList);
+            }
+        }
+
+        System.out.println("Максимальная стоимость рюкзака: " + maxPrice);
+        for(Item ii : maxPriceList)
+            System.out.println(ii.getName() + " : " + ii.getPrice() + " : " + ii.getWeight() + " кг");
+    }
+
+    private static List<Item> copyOf2(List<Item> priceList) {
+        List<Item> result = new ArrayList<>();
+        for(int i = 0; i < priceList.size(); i ++)
+            result.add(priceList.get(i));
+        return result;
+    }
+
+    public static void main(String[] args) {
+        new BackPack(makeItems()).getRandomItems();
+        findExpensive();
     }
 }
