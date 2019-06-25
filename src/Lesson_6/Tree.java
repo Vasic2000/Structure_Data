7c3fd26f786c27e6a04a5584b0ef29a7aa38dc77
Vasic2000 <Vasic2000@yandex.ru>
2019-06-25 12:08:40 +0300

    Повторил урок 6!

---
+++ src/Lesson_6/Tree.java
@@ -0,0 +1,23 @@
+package Lesson_6;
+
+public interface Tree<E> {
+    enum TraverseMode {
+        IN_ORDER,
+        PRE_ORDER,
+        POST_ORDER
+    }
+
+    boolean add(E value);
+
+    boolean find(E value);
+
+    boolean remove(E value);
+
+    boolean isEmpty();
+
+    int size();
+
+    void traverse(TraverseMode mode);
+
+    void display();
+}
