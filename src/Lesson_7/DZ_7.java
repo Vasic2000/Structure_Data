94fac2e6c80a531557a60cd6b84af548c5601012
Vasic2000 <Vasic2000@yandex.ru>
2019-06-28 15:41:23 +0300

    Готово. Чуть усложнил карту. Несколько тупиков.

--- src/Lesson_7/DZ_7.java
+++ src/Lesson_7/DZ_7.java
@@ -3,15 +3,17 @@ package Lesson_7;
 public class DZ_7 {
     public static void main(String[] args) {
         Graph road_map = makeGraph();
-        road_map.bfs("Москва");
-        System.out.println("----------------------------");
-        road_map.dfs("Москва");
+//        road_map.bfs("Москва");
+//        System.out.println("----------------------------");
+//        road_map.dfs("Москва");
+
+        road_map.findBFS("Саратов", "Новомосковск");
     }
 
 
 
     private static Graph makeGraph() {
-        Graph graph = new Graph(10);
+        Graph graph = new Graph(13);
         graph.addVertex("Москва");
         graph.addVertex("Тула");
         graph.addVertex("Липецк");
@@ -23,6 +25,13 @@ public class DZ_7 {
         graph.addVertex("Орёл");
         graph.addVertex("Курск");
 
+        //Для проверки, города прицеплены к Туле
+        graph.addVertex("Новомосковск");
+        graph.addVertex("Узловая");
+
+        //Для проверки, города прицеплен к Липецку
+        graph.addVertex("Елец");
+
         graph.addEdges("Москва", "Тула", "Рязань", "Калуга");
         graph.addEdges("Воронеж", "Липецк", "Саратов", "Курск");
         graph.addEdge("Тула", "Липецк");
@@ -30,6 +39,14 @@ public class DZ_7 {
         graph.addEdge("Тамбов", "Саратов");
         graph.addEdge("Калуга", "Орёл");
         graph.addEdge( "Орёл", "Курск");
+
+        //Для проверки, города прицеплены к Липецку и ещё связаны
+        graph.addEdge("Тула", "Новомосковск");
+        graph.addEdge("Тула", "Узловая");
+        graph.addEdge("Новомосковск", "Узловая");
+
+        graph.addEdge("Липецк", "Елец");
+
 
         return graph;
     }
